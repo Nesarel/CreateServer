ee46e91a2be5b3f79c17d57044bba5726b372da2
Nesarel <nesarel.gd@gmail.com>
2022-10-03 16:49:06 +0200

    last commit

--- controller/code.js
+++ controller/code.js
@@ -2,11 +2,12 @@ const knex = require('../model/knex')
 
 async function getOneCode(id){
 
-    // const data = await knex.raw(`select * from html where id = ?`, id)
-    const data = await knex('html')
-    .select('*')
-    .where({id})
-    return data
+    const data = await knex.raw(`SELECT * FROM html WHERE id = ?`, [id])
+
+    // const data = await knex('html')
+    // .select('*')
+    // .where({id})
+    return data.rows
 }
 
 module.exports = {
